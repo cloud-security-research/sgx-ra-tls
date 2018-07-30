e24a91be56d9245f3090316bae7e456d83d6c183
Thomas Knauth <thomas.knauth@intel.com>
2018-07-30 14:43:29 -0700

    Remove hard-coded SPID. Make quote-type (linkable vs unlinkable) configurable.

--- ra_tls_options.c
+++ ra_tls_options.c
@@ -5,6 +5,7 @@ struct ra_tls_options my_ra_tls_options = {
               0x00, 0x00, 0x00, 0x00,
               0x00, 0x00, 0x00, 0x00,
               0x00, 0x00, 0x00, 0x00}},
+    .quote_type = SGX_UNLINKABLE_SIGNATURE,
     .ias_key_file = "./ias-client-key.pem",
     .ias_cert_file = "./ias-client-cert.pem",
     .ias_server = "test-as.sgx.trustedservices.intel.com:443"
