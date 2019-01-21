f46e719dc102901ae29bac55cf6b930d03318317
njriasan <njriasanovsky@berkeley.edu>
2019-01-21 01:26:09 -0800

    added support for self signed certs by relying on openssl rather than libressl

--- sgxlkl/https-server/buildenv.sh
+++ sgxlkl/https-server/buildenv.sh
@@ -7,4 +7,12 @@ PATH=/usr/sbin:/sbin:/usr/bin:/bin
 cd /home
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.6/community" >> /etc/apk/repositories
 apk update
-apk add python
+apk add openssl
+apk add openssl-dev
+apk add make
+apk add build-base
+apk update && apk add ca-certificates wget && update-ca-certificates
+apk add openssl
+wget https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz 
+tar -xvzf Python-2.7.15.tgz
+cd Python-2.7.15/ && ./configure && make && make install && mv python /usr/bin
