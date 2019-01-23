8c441c9f2a137746cb02c08433d52973e0e32f24
Thomas Knauth <thomas.knauth@intel.com>
2019-01-23 11:11:51 -0800

    Fix conversion of DER RSA key into PEM (#8).

--- sgxlkl/https-server/https-server.py
+++ sgxlkl/https-server/https-server.py
@@ -8,6 +8,19 @@ import base64
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
+def rsa_key_der_to_pem(key_der):
+    out = '-----BEGIN RSA PRIVATE KEY-----\n'
+    i = 0
+    for c in base64.b64encode(key_der):
+        if (i == 64):
+            out += '\n'
+            i = 0
+        out += c
+        i += 1
+    out += '\n'
+    out += '-----END RSA PRIVATE KEY-----'
+    return out
+
 # The RA-TLS library currently only exposes the key and certificate as
 # in DER format. The Python API expects them in PEM format. Hence, we
 # convert them here.
@@ -16,12 +29,8 @@ f = open('/tmp/crt.pem', 'w')
 f.write(crt_pem)
 f.close()
 
-key_b64 = base64.b64encode(open('/tmp/key').read())
-f = open('/tmp/key.pem', 'w')
-print >> f, '-----BEGIN RSA PRIVATE KEY-----'
-print >> f, key_b64
-print >> f, '-----END RSA PRIVATE KEY-----'
-f.close()
+with open('/tmp/key.pem', 'w') as f:
+    print >> f, rsa_key_der_to_pem(open('/tmp/key').read())
 
 # Start the HTTPS web server
 ip = '10.0.1.1'
