c495b1cf6516f24f5204c91c5e7eaa3c2541a4ea
Thomas Knauth <thomas.knauth@intel.com>
2019-09-12 14:52:44 -0700

    Make quote type (linkable vs unlinkable) be configurable.

--- ra_tls_options.c.sh
+++ ra_tls_options.c.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+# set -x
 
 if [[ -z "$ECDSA_SUBSCRIPTION_KEY" ]] && ( [[ -z "$SPID" ]] || [[ -z "$EPID_SUBSCRIPTION_KEY" ]] ); then
     echo "Either SPID and EPID_SUBSCRIPTION_KEY or ECDSA_SUBSCRIPTION_KEY is required!"
@@ -13,6 +13,12 @@ if ( [[ ! -z "$SPID" ]] && [[ -z "$EPID_SUBSCRIPTION_KEY" ]] ) || \
     exit 1
 fi
 
+if ( [[ "$QUOTE_TYPE" != "SGX_LINKABLE_SIGNATURE" ]] ) && \
+   ( [[ "$QUOTE_TYPE" != "SGX_UNLINKABLE_SIGNATURE" ]] ); then
+    echo "QUOTE_TYPE must be one of SGX_UNLINKABLE_SIGNATURE or SGX_LINKABLE_SIGNATURE"
+    exit 1
+fi
+
 SPID_BYTE_ARRAY=$(echo $SPID | python -c 'import sys ; s = sys.stdin.readline().strip(); print("".join(["0x"+s[2*i:2*i+2]+"," for i in range(len(s)/2)]))')
 
 cat <<HEREDOC
@@ -21,7 +27,7 @@ cat <<HEREDOC
 struct ra_tls_options my_ra_tls_options = {
     // SPID format is 32 hex-character string, e.g., 0123456789abcdef0123456789abcdef
     .spid = {{$SPID_BYTE_ARRAY}},
-    .quote_type = SGX_UNLINKABLE_SIGNATURE,
+    .quote_type = $QUOTE_TYPE,
     .ias_server = "api.trustedservices.intel.com/sgx/dev",
     // EPID_SUBSCRIPTION_KEY format is "012345679abcdef012345679abcdef"
     .subscription_key = "$EPID_SUBSCRIPTION_KEY"
